be1bd796b7b9a2f4b1b2d540be8283302a1f7c45
Tizzy Saurus <tizzysaurus@gmail.com>
2025-10-28 22:12:03 +0000

    Initial Commit (flake + home-manager + zsh + vscode basics)

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "NixOS configuration for nixos-sir";
+
+  inputs = {
+    # You can pin nixpkgs to a specific branch or commit if desired
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+    # Optionally include home-manager or other flakes
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.nixos-sir = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        # Your system configuration file
+        ./configuration.nix
+
+
+        ## Integrate Home Manager as a NixOS module
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.tizzysaurus = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,126 @@
+{ config, pkgs, ... }:
+
+# TODO: Investigate https://nix-community.github.io/home-manager/options.xhtml
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "tizzysaurus";
+  home.homeDirectory = "/home/tizzysaurus";
+
+  nixpkgs.config.allowUnfree = true;
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    bat
+    btop
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "Tizzy Saurus";
+        email = "tizzysaurus@gmail.com";
+      };
+    };
+  };
+
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscode;
+
+    profiles.default.extensions = with pkgs.vscode-extensions; [
+      usernamehw.errorlens
+      esbenp.prettier-vscode
+      ms-python.python
+      bbenoist.nix
+      dbaeumer.vscode-eslint
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "pretty-ts-errors";
+        publisher = "yoavbls";
+        version = "0.6.1";
+        sha256 = "sha256-LvX21nEjgayNd9q+uXkahmdYwzfWBZOhQaF+clFUUF4=";
+      }
+      {
+        name = "vs-code-prettier-eslint";
+        publisher = "rvest";
+        version = "6.0.0";
+        sha256 = "sha256-PogNeKhIlcGxUKrW5gHvFhNluUelWDGHCdg5K+xGXJY=";
+      }
+    ];
+
+    #userSettings = {
+    #  "editor.tabSize" = 2
+    #}
+  };
+
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+  };
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/tizzysaurus/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+}
